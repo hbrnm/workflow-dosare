19fa82f6bbea33a79783352ef048c90ae9f02a34
hbrnm <laudat.alexandru@gmail.com>
2026-07-30 12:23:24 +0300

    Update ClaimModal.jsx

--- src/components/modals/ClaimModal.jsx
+++ src/components/modals/ClaimModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useMemo } from "react";
 import {
   FileText, FileDown, Copy, X, ShieldCheck, History, Loader2, Car, Phone, MessageCircle,
-  Clock, AlertOctagon, Wrench, Paintbrush, ImageIcon, Upload, Trash2, Save, MessageSquare
+  Clock, AlertOctagon, Wrench, Paintbrush, ImageIcon, Upload, Trash2, Save, MessageSquare, Plus
 } from "lucide-react";
 import { STATUSES, INSURERS, getStatusDefinition } from "../../constants/config";
 import { fmtDate, fmtDateTime, daysBetween, nowISO, telLink, waLink, uid, fmtProgramare } from "../../utils/dateUtils";
