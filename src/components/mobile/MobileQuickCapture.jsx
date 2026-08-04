f78985a35f847e162670cefe2f0ce582910129d4
hbrnm <laudat.alexandru@gmail.com>
2026-08-04 23:13:05 +0300

    Update MobileQuickCapture.jsx

--- src/components/mobile/MobileQuickCapture.jsx
+++ src/components/mobile/MobileQuickCapture.jsx
@@ -954,6 +954,8 @@ export default function MobileQuickCapture({ claims, onOpen, onPatch, canEditFn,
           </button>
           <img src={previewMedia} alt="Previzualizare" className="max-w-full max-h-[85vh] object-contain rounded-xl shadow-2xl" />
         </div>
+      )}
+
       {/* MODAL VIZOR CAMERĂ FOTO CONTINUĂ (RAFALĂ Pe MOBIL) */}
       {showContinuousCamera && selectedClaim && (
         <ContinuousCameraModal
