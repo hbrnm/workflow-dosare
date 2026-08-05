3b513ab72a97e5f732284b1caf61487761ded678
hbrnm <200203490+hbrnm@users.noreply.github.com>
2026-08-05 06:53:37 +0300

    vite plugin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { registerSW } from 'virtual:vite-plugin-pwa/register'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+registerSW({ immediate: true })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
