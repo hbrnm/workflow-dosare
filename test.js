90784bd3c5b223bd230554a85784244a697ed579
Laudat Alexandru <laudat.alexandru@gmail.com>
2026-09-02 12:34:53 +0300

    chore: remove test scripts

--- test.js
+++
@@ -1,9 +0,0 @@
-﻿const contractTextParts = [
-  { t: "a. Autoturismul întrunește toate condițiile tehnice de folosire, fiind în stare perfectă de funcționare și neavând defecte și lipsuri." },
-  { t: "b. Perioada minimă de închiriere este de 24 ore, o zi indivizibilă." },
-  { t: "c. Chiriașul are obligația ca pentru autoturismul pus la dispoziție de către proprietar să plătească o chirie echivalentă în lei la cursul de schimb al BNR din ziua efectuării plății." },
-  { t: "d. În afara sumelor pe care le plătește proprietarului drept chirie, chiriașul este obligat să constituie un depozit în numerar și în echivalent, ca și garanție pentru bună utilizare pe durata derulării contractului, sumă ce va fi returnată chiriașului, dacă nu sunt constatate daune sau avarii la autoturism, în momentul predării acestuia către proprietar." },
-  { t: "e. Chiriașul se obligă să folosească autoturismul închiriat cu diligența unui bun proprietar." },
-  { t: "f. La orice lipsă a obiectelor de inventar menționate în procesul verbal de predare/ primire al autoturismului (care face parte integrantă din prezentul contract) societatea își rezervă dreptul de a le reține valoarea din garanția depusă." },
-  { t: "g. Tariful de închiriere nu cuprinde combustibilul, taxele de trecere / parcare, garaj, transbordare pe timpul închirierii." }
-];
